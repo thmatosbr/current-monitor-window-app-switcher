d737ce98a193907076403c42d1b558cffe6a37b6
Thiago Brasil <th.matos.br@gmail.com>
2024-10-28 00:49:25 -0400

    Filters window switcher items by current monitor

--- extension.js
+++ extension.js
@@ -1,26 +1,19 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
-import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import { WindowSwitcherPopup } from 'resource:///org/gnome/shell/ui/altTab.js';
 
-export default class PlainExampleExtension extends Extension {
+let original = {};
+
+export default class CurrentMonitorWindowAppSwitcher extends Extension {
     enable() {
+        original['_getWindowList'] = WindowSwitcherPopup.prototype._getWindowList;
+
+        WindowSwitcherPopup.prototype._getWindowList = function () {
+            let windows = original['_getWindowList'].apply(this, arguments);
+            return windows.filter(w => w.get_monitor() === global.display.get_current_monitor());
+        };
     }
 
     disable() {
+        WindowSwitcherPopup.prototype._getWindowList = original['_getWindowList'];
     }
 }
