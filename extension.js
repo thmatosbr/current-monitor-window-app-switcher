a22939501352ca81969509f9bb78aed38c6e7f8a
Thiago Brasil <th.matos.br@gmail.com>
2024-10-28 00:57:24 -0400

    Filters app switcher items by current monitor

--- extension.js
+++ extension.js
@@ -1,19 +1,32 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
-import { WindowSwitcherPopup } from 'resource:///org/gnome/shell/ui/altTab.js';
+import { AppSwitcherPopup, WindowSwitcherPopup } from 'resource:///org/gnome/shell/ui/altTab.js';
 
 let original = {};
 
 export default class CurrentMonitorWindowAppSwitcher extends Extension {
     enable() {
         original['_getWindowList'] = WindowSwitcherPopup.prototype._getWindowList;
+        original['_init'] = AppSwitcherPopup.prototype._init;
 
         WindowSwitcherPopup.prototype._getWindowList = function () {
             let windows = original['_getWindowList'].apply(this, arguments);
             return windows.filter(w => w.get_monitor() === global.display.get_current_monitor());
         };
+
+        AppSwitcherPopup.prototype._init = function () {
+            original['_init'].apply(this, arguments);
+
+            let items = [...this._items];
+            items.forEach(item => {
+                if (!item.cachedWindows.some(w => w.get_monitor() === global.display.get_current_monitor())) {
+                    this._switcherList._removeIcon(item.app);
+                }
+            });
+        };
     }
 
     disable() {
         WindowSwitcherPopup.prototype._getWindowList = original['_getWindowList'];
+        AppSwitcherPopup.prototype._init = original['_init'];
     }
 }
