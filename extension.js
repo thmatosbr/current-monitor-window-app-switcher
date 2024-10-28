184a9712da65dcc958da675406c09dfd31f4e383
Thiago Brasil <th.matos.br@gmail.com>
2024-10-28 01:44:51 -0400

    Show the switcher pop-up on the current monitor

--- extension.js
+++ extension.js
@@ -1,5 +1,8 @@
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import { AppSwitcherPopup, WindowSwitcherPopup } from 'resource:///org/gnome/shell/ui/altTab.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+const Gi = imports._gi;
 
 let original = {};
 
@@ -7,6 +10,8 @@ export default class CurrentMonitorWindowAppSwitcher extends Extension {
     enable() {
         original['_getWindowList'] = WindowSwitcherPopup.prototype._getWindowList;
         original['_init'] = AppSwitcherPopup.prototype._init;
+        original['windowSwitcherAllocate'] = WindowSwitcherPopup.prototype.vfunc_allocate;
+        original['appSwitcherAllocate'] = AppSwitcherPopup.prototype.vfunc_allocate;
 
         WindowSwitcherPopup.prototype._getWindowList = function () {
             let windows = original['_getWindowList'].apply(this, arguments);
@@ -23,10 +28,27 @@ export default class CurrentMonitorWindowAppSwitcher extends Extension {
                 }
             });
         };
+
+        const overrideAllocate = function (proto, originalMethod) {
+            proto[Gi.gobject_prototype_symbol][Gi.hook_up_vfunc_symbol](
+                'allocate', function () {
+                    let originalPrimaryMonitor = Main.layoutManager.primaryMonitor;
+                    Main.layoutManager.primaryMonitor = Main.layoutManager.currentMonitor;
+                    original[originalMethod].apply(this, arguments);
+                    Main.layoutManager.primaryMonitor = originalPrimaryMonitor;
+                });
+        }
+
+        overrideAllocate(WindowSwitcherPopup.prototype, 'windowSwitcherAllocate');
+        overrideAllocate(AppSwitcherPopup.prototype, 'appSwitcherAllocate');
     }
 
     disable() {
         WindowSwitcherPopup.prototype._getWindowList = original['_getWindowList'];
         AppSwitcherPopup.prototype._init = original['_init'];
+        WindowSwitcherPopup.prototype[Gi.gobject_prototype_symbol][Gi.hook_up_vfunc_symbol](
+            'allocate', original['windowSwitcherAllocate']);
+        AppSwitcherPopup.prototype[Gi.gobject_prototype_symbol][Gi.hook_up_vfunc_symbol](
+            'allocate', original['appSwitcherAllocate']);
     }
 }
